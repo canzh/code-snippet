c232cd85ddde8b261bbbc51ac6394644d94f194c
can zhang <can.zhang@tr.com>
2019-07-19 19:16:50 +0800

    two pointer

---
+++ two-pointer.java
@@ -0,0 +1,28 @@
+public class CodeSnippet {
+    public void sortColors(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return;
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        for (int i = low; i <= high;) {
+            if (nums[i] == 0) {
+                swap(nums, i, low);
+                low++;
+                i++;
+            } else if (nums[i] == 2) {
+                swap(nums, i, high);
+                high--;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int low, int mid) {
+        int temp = nums[low];
+        nums[low] = nums[mid];
+        nums[mid] = temp;
+    }
+}

1e5b66e62fbcd90e6bcaacb88a043e3c6e864a41
can zhang <can.zhang@tr.com>
2019-07-23 18:40:15 +0800

    morris traversal tree

--- tree-postorder-iterative.java
+++ tree-postorder-iterative.java
@@ -15,8 +15,8 @@ public class CodeSnippet {
         Stack<TreeNode> stack = new Stack<>();
         TreeNode p = root;
 
-        while(!stack.isEmpty() || p != null) {
-            while(p != null) {
+        while (!stack.isEmpty() || p != null) {
+            while (p != null) {
                 stack.push(p);
 
                 // visit p for logic
@@ -33,8 +33,8 @@ public class CodeSnippet {
         Stack<TreeNode> stack = new Stack<>();
         TreeNode p = root;
 
-        while(!stack.isEmpty() || p != null) {
-            while(p != null) {
+        while (!stack.isEmpty() || p != null) {
+            while (p != null) {
                 stack.push(p);
                 p = p.left;
             }
@@ -58,5 +58,52 @@ public class CodeSnippet {
         preOrderRecursive(root.right);
 
         // logic before return back to parent node
+    }
+
+    // leetcode 114, used to flatten b-tree to linked list
+    public void postOrderRightToLeft(TreeNode root) {
+        if (root == null)
+            return;
+
+        postOrderRightToLeft(root.right);
+        postOrderRightToLeft(root.left);
+
+        // visit root
+    }
+
+    // morris traversal is in-order
+    public void morrisTraversal(TreeNode root) {
+        if (root == null)
+            return;
+
+        TreeNode cur = root, pre;
+
+        while (cur != null) {
+            if (cur.left == null) {
+                System.out.print(cur.val + " ");
+                cur = cur.right;
+            } else {
+                /* Find the inorder predecessor of current */
+                pre = cur.left;
+                while (pre.right != null && pre.right != cur) {
+                    pre = pre.right;
+                }
+
+                /* Make current as right child of its inorder predecessor */
+                if (pre.right == null) {
+                    pre.right = cur;
+                    cur = cur.left;
+                }
+                /*
+                 * Revert the changes made in the 'if' part to restore the original tree i.e.,
+                 * fix the right child of predecessor
+                 */
+                else {
+                    pre.right = null;
+                    System.out.print(cur.val + " ");
+                    cur = cur.right;
+                }
+            }
+        }
     }
 }

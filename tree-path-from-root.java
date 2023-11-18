bfa6be37afb0096860b3c6e96fe125d045b18d80
can zhang <mylostday@126.com>
2023-11-18 10:14:55 +0800

    add tree lowestCommonAncestor

--- tree-path-from-root.java
+++ tree-path-from-root.java
@@ -29,3 +29,70 @@ public class CodeSnippet {
         return found;
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == root || q == root)
+            return root;
+
+        List<TreeNode> p1 = new LinkedList<>();
+        List<TreeNode> p2 = new LinkedList<>();
+
+        getPathFromRoot(root, p, p1);
+        getPathFromRoot(root, q, p2);
+
+        return findDiffPoint(p1, p2);
+    }
+
+    boolean getPathFromRoot(TreeNode root, TreeNode p, List<TreeNode> path) {
+        if (root == null)
+            return false;
+
+        path.add(root);
+        
+        if (root.val == p.val)
+            return true;
+
+
+        if (getPathFromRoot(root.left, p, path))
+            return true;
+        if (getPathFromRoot(root.right, p, path))
+            return true;
+
+        path.remove(path.size() - 1);
+
+        return false;
+    }
+
+    TreeNode findDiffPoint(List<TreeNode> l1, List<TreeNode> l2) {
+        int len = Math.min(l1.size(), l2.size());
+        TreeNode p = null;
+        for (int i = 0; i < len; i++) {
+            if (l1.get(i) == l2.get(i)) {
+                p = l1.get(i);
+            } else {
+                break;
+            }
+        }
+        return p;
+    }
+
+
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        return left == null ? right : right == null ? left : root;
+    }
+}
